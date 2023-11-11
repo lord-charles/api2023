abfd8f86f7666784245002f03ea745a3d9abed26
lord-charles <mwanikicharles226@gmail.com>
2023-11-11 13:17:56 +0300

    43

--- controller/customerService.js
+++ controller/customerService.js
@@ -1,21 +1,15 @@
 const axios = require("axios");
 
 const tidioCs = async (req, res) => {
-  let config = {
-    method: "get",
-    maxBodyLength: Infinity,
-    url: "https://widget-v4.tidiochat.com/1_199_0/static/js/render.2d14872ff6b53ecaadcb.js",
-    headers: {},
-  };
-
-  axios
-    .request(config)
-    .then((response) => {
-      res.send(JSON.stringify(response.data));
-    })
-    .catch((error) => {
-      console.log(error);
-    });
+  try {
+    const response = await axios.get(
+      "https://widget-v4.tidiochat.com/1_199_0/static/js/render.2d14872ff6b53ecaadcb.js"
+    );
+    // console.log(response.data);
+    res.send(response.data);
+  } catch (err) {
+    console.log(err);
+  }
 };
 
 
